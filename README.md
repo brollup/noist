a93aba4995f96920fee159905eb58c9ba499c51c
brqgoo <100725376+brqgoo@users.noreply.github.com>
2024-09-14 20:33:19 +0300

    vse_encrypt & vse_decrypt

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 NOIST is a non-interactive, single-round t-of-n threshold signing scheme.
 
 NOIST allows multiple untrusted entities to come together and jointly produce a group key and generate signatures in constant time, where a disruptive signer cannot force a re-do of the entire round. The resulting signature is a single 64-byte  [BIP-340](https://github.com/bitcoin/bips/blob/master/bip-0340.mediawiki)  compatible Schnorr signature.
+
+> [!NOTE]
+> Work in progress.
--- src/vse.rs
+++ src/vse.rs
@@ -1,4 +1,5 @@
-use secp::{Point, Scalar};
+use crate::into::SecpError;
+use secp::{MaybeScalar, Point, Scalar};
 
 pub fn vse_encryption_secret(self_secret: Scalar, to_public: Point) -> Scalar {
     let secret_point = self_secret * to_public;
@@ -20,4 +21,24 @@ pub fn vse_encryption_public(self_secret: Scalar, to_public: Point) -> Point {
     let shared_public = shared_secret.base_point_mul();
 
     shared_public
+}
+
+pub fn vse_encrypt(
+    secret_to_encrypt: Scalar,
+    encryption_secret: Scalar,
+) -> Result<Scalar, SecpError> {
+    match secret_to_encrypt + encryption_secret {
+        MaybeScalar::Valid(scalar) => Ok(scalar),
+        MaybeScalar::Zero => Err(SecpError::InvalidScalar),
+    }
+}
+
+pub fn vse_decrypt(
+    secret_to_decrypt: Scalar,
+    encryption_secret: Scalar,
+) -> Result<Scalar, SecpError> {
+    match secret_to_decrypt - encryption_secret {
+        MaybeScalar::Valid(scalar) => Ok(scalar),
+        MaybeScalar::Zero => Err(SecpError::InvalidScalar),
+    }
 }
