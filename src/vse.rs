bf27e0a908aa645521dc303466b72bfc382905e3
brqgoo <100725376+brqgoo@users.noreply.github.com>
2024-09-14 20:44:01 +0300

    encrypting_key

--- src/vse.rs
+++ src/vse.rs
@@ -1,7 +1,7 @@
 use crate::into::SecpError;
 use secp::{MaybePoint, MaybeScalar, Point, Scalar};
 
-pub fn vse_encryption_secret(self_secret: Scalar, to_public: Point) -> Scalar {
+pub fn vse_encrypting_key_secret(self_secret: Scalar, to_public: Point) -> Scalar {
     let secret_point = self_secret * to_public;
 
     let secret_point_xbytes = secret_point.serialize_xonly();
@@ -11,23 +11,15 @@ pub fn vse_encryption_secret(self_secret: Scalar, to_public: Point) -> Scalar {
     shared_secret
 }
 
-pub fn vse_encryption_public(self_secret: Scalar, to_public: Point) -> Point {
-    let secret_point = self_secret * to_public;
-
-    let secret_point_xbytes = secret_point.serialize_xonly();
-
-    let shared_secret = Scalar::reduce_from(&secret_point_xbytes);
-
-    let shared_public = shared_secret.base_point_mul();
-
-    shared_public
+pub fn vse_encrypting_key_public(self_secret: Scalar, to_public: Point) -> Point {
+    vse_encrypting_key_secret(self_secret, to_public).base_point_mul()
 }
 
 pub fn vse_encrypt(
     secret_to_encrypt: Scalar,
-    encryption_secret: Scalar,
+    encrypting_key_secret: Scalar,
 ) -> Result<Scalar, SecpError> {
-    match secret_to_encrypt + encryption_secret {
+    match secret_to_encrypt + encrypting_key_secret {
         MaybeScalar::Valid(scalar) => Ok(scalar),
         MaybeScalar::Zero => Err(SecpError::InvalidScalar),
     }
@@ -35,9 +27,9 @@ pub fn vse_encrypt(
 
 pub fn vse_decrypt(
     secret_to_decrypt: Scalar,
-    encryption_secret: Scalar,
+    encrypting_key_secret: Scalar,
 ) -> Result<Scalar, SecpError> {
-    match secret_to_decrypt - encryption_secret {
+    match secret_to_decrypt - encrypting_key_secret {
         MaybeScalar::Valid(scalar) => Ok(scalar),
         MaybeScalar::Zero => Err(SecpError::InvalidScalar),
     }
@@ -46,12 +38,12 @@ pub fn vse_decrypt(
 pub fn vse_verify(
     encrypted_share_scalar: Scalar,
     public_share_point: Point,
-    encryption_point: Point,
+    encrypting_key_public: Point,
 ) -> bool {
     let combined_point = encrypted_share_scalar.base_point_mul();
 
     combined_point
-        == match public_share_point + encryption_point {
+        == match public_share_point + encrypting_key_public {
             MaybePoint::Valid(point) => point,
             MaybePoint::Infinity => return false,
         }
